9d10c1c38cb7569725f2b6fe1afb62f08dc34693
Yxin-KaiByz <100540976+Yxin-KaiByz@users.noreply.github.com>
2023-07-15 17:53:44 +1200

    Added Character selection UI before game start

---
+++ Assets/Scripts/Menu/SlidingMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingMenu: MonoBehaviour
+{
+    private Animator slideAnimator;
+    private static SlidingMenu instance;
+    public static SlidingMenu Instance => instance;
+    private void Awake()
+    {
+        slideAnimator = GetComponent<Animator>();
+        instance = this;
+    }
+    
+    public void SlideLeft()
+    {
+        slideAnimator.SetTrigger("Left");
+    }
+    public void SlideRight()
+    {
+        slideAnimator.SetTrigger("Right");
+    }
+}
